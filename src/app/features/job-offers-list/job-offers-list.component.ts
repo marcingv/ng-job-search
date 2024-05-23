6d5e08e2fd986a7c44293be0a7e5e771d49cdd03
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-23 08:05:21 +0200

    Empty message displayed for empty job offers lists.

--- src/app/features/job-offers-list/job-offers-list.component.ts
+++ src/app/features/job-offers-list/job-offers-list.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { JobOffer } from '@core/types';
 import { JobOfferListItemComponent } from './job-offer-list-item/job-offer-list-item.component';
+import { EmptyCollectionPlaceholderComponent } from 'src/app/ui/placeholders/empty-collection-placeholder';
 
 @Component({
   selector: 'app-job-offers-list',
   standalone: true,
-  imports: [JobOfferListItemComponent],
+  imports: [JobOfferListItemComponent, EmptyCollectionPlaceholderComponent],
   templateUrl: './job-offers-list.component.html',
   styleUrl: './job-offers-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,4 +14,6 @@ import { JobOfferListItemComponent } from './job-offer-list-item/job-offer-list-
 export class JobOffersListComponent {
   @Input({ required: true }) public data: JobOffer[] = [];
   @Input() public showActions: boolean = true;
+  @Input() public emptyMessage?: string;
+  @Input() public isLoading: boolean = false;
 }
